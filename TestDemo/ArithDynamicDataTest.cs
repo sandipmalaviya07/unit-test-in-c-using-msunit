577304e7d55a04b67bdab23957d5fdd699b1c84c
Ajay Thakor <ajaykumar.thakor@samarpaninfotech.co.in>
2022-12-02 14:47:06 +0530

    initial commit

---
+++ TestDemo/ArithDynamicDataTest.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTestDemo.Services;
+
+namespace TestDemo
+{
+    [TestClass]
+    public class ArithDynamicDataTest
+    {
+        ArithService arith = new ArithService();
+
+        [DataTestMethod]
+        [DynamicData(nameof(AddData), DynamicDataSourceType.Method)]
+        public void Add(int x, int y, int expected)
+        {
+            int r = arith.add(x, y);
+            Assert.AreEqual(r, expected);
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(SubData), DynamicDataSourceType.Method)]
+        public void Sub(int x, int y, int expected)
+        {
+            int r = arith.sub(x, y);
+            Assert.AreEqual(r, expected);
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(MulData), DynamicDataSourceType.Method)]
+        public void Mul(int x, int y, int expected)
+        {
+            int r = arith.mul(x, y);
+            Assert.AreEqual(r, expected);
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(DivData), DynamicDataSourceType.Method)]
+        public void Div(int x, int y, int expected)
+        {
+            int r = arith.div(x, y);
+            Assert.AreEqual(r, expected);
+        }
+
+        private static IEnumerable<object[]> AddData()
+        {
+            return new[]
+            {
+            new object[] { 1, 2, 3 },
+            new object[] { 2, 2, 4 },
+            new object[] { -1, 4, 3 }
+        };
+        }
+
+        private static IEnumerable<object[]> SubData()
+        {
+            return new[]
+            {
+            new object[] { 1, 2, -1 },
+            new object[] { 2, 2, 0 },
+            new object[] { 3, 2, 1 }
+        };
+        }
+
+        private static IEnumerable<object[]> MulData()
+        {
+            return new[]
+            {
+            new object[] { 9, 3, 27 },
+            new object[] { 3, 3, 9 },
+            new object[] { -3, -3, 9 }
+        };
+        }
+
+        private static IEnumerable<object[]> DivData()
+        {
+            return new[]
+            {
+            new object[] { 9, 3, 3 },
+            new object[] { 3, 3, 1 },
+            new object[] { 8, 2, 4 }
+        };
+        }
+    }
+}
---
+++ UnitTestDemo/Services/MessageService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UnitTestDemo.Services
+{
+    public class MessageService
+    {
+        public Func<string> msg1 = () => "Hello there!";
+        public Func<string> msg2 = () => "Good Morning!";        
+    }
+}
